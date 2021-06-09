1f267e92c0ffcf1e791ba564c2bbefd36007a267
Kilian S <kilian.seelbach@gmail.com>
2021-06-10 00:26:04 +0200

    added Bone, change tap#, flip enc, power down oled

--- keymap.c
+++ keymap.c
@@ -1,8 +1,10 @@
 #include QMK_KEYBOARD_H
+#define TAPPING_TOGGLE 2
 
 enum sofle_layers {
     /* _M_XYZ = Mac Os, _W_XYZ = Win/Linux */
     _QWERTY,
+    _BONE,
     _COLEMAK,
     _LOWER,
     _RAISE,
@@ -47,29 +49,38 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_MUTE, KC_MPLY, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_ENT), \
   KC_LGUI, KC_LALT, KC_LCTL, MO(2), KC_SPC, KC_ENT, TT(3), KC_RCTL, KC_RALT, KC_RGUI \
 ),
-/*
- * COLEMAK
- * ,-----------------------------------------.                    ,-----------------------------------------.
- * |  `   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  `   |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | ESC  |   Q  |   W  |   F  |   P  |   G  |                    |   J  |   L  |   U  |   Y  |   ;  | Bspc |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | TAB  |   A  |   R  |   S  |   T  |   D  |-------.    ,-------|   H  |   N  |   E  |   I  |   O  |  '   |
- * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
- * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   K  |   M  |   ,  |   .  |   /  |RShift|
- * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            | LGUI | LAlt | LCTR |LOWER | /Enter  /       \Space \  |RAISE | RCTR | RAlt | RGUI |
- *            |      |      |      |      |/       /         \      \ |      |      |      |      |
- *            `----------------------------------'           '------''---------------------------'
- */
 
-[_COLEMAK] = LAYOUT( \
-  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_GRV, \
-  KC_ESC,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_G,                      KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN,  KC_BSPC, \
-  KC_TAB,   KC_A,   KC_R,    KC_S,    KC_T,    KC_D,                      KC_H,    KC_N,    KC_E,    KC_I,    KC_O,  KC_QUOT, \
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,      KC_MPLY ,KC_K,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT, \
-                 KC_LGUI,KC_LALT,KC_LCTRL,KC_LOWER, KC_ENT,      KC_SPC,  KC_RAISE, KC_RCTRL, KC_RALT, KC_RGUI \
+[_BONE] = LAYOUT( \
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
+  KC_TRNS, KC_J, KC_D, KC_U, KC_A, KC_X,                    KC_P, KC_H, KC_L, KC_M, KC_W, KC_MINS, \
+  KC_TRNS, KC_C, KC_T, KC_I, KC_E, KC_O,                    KC_B, KC_N, KC_R, KC_S, KC_G, KC_Q, \
+  KC_TRNS, KC_F, KC_V, KC_SCLN, KC_QUOT, KC_GRV, KC_TRNS,   KC_TRNS, KC_Z, KC_Y, KC_COMM, KC_DOT, KC_K, KC_TRNS, \
+  KC_LGUI, KC_LALT, KC_LCTL, MO(2), KC_SPC,                 KC_ENT, TT(3), KC_LCTL, KC_LALT, KC_LGUI \
 ),
+
+// /*
+//  * COLEMAK
+//  * ,-----------------------------------------.                    ,-----------------------------------------.
+//  * |  `   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  `   |
+//  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+//  * | ESC  |   Q  |   W  |   F  |   P  |   G  |                    |   J  |   L  |   U  |   Y  |   ;  | Bspc |
+//  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+//  * | TAB  |   A  |   R  |   S  |   T  |   D  |-------.    ,-------|   H  |   N  |   E  |   I  |   O  |  '   |
+//  * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
+//  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   K  |   M  |   ,  |   .  |   /  |RShift|
+//  * `-----------------------------------------/       /     \      \-----------------------------------------'
+//  *            | LGUI | LAlt | LCTR |LOWER | /Enter  /       \Space \  |RAISE | RCTR | RAlt | RGUI |
+//  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
+//  *            `----------------------------------'           '------''---------------------------'
+//  */
+
+// [_COLEMAK] = LAYOUT( \
+//   KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_GRV, \
+//   KC_ESC,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_G,                      KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN,  KC_BSPC, \
+//   KC_TAB,   KC_A,   KC_R,    KC_S,    KC_T,    KC_D,                      KC_H,    KC_N,    KC_E,    KC_I,    KC_O,  KC_QUOT, \
+//   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,      KC_MPLY ,KC_K,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT, \
+//                  KC_LGUI,KC_LALT,KC_LCTRL,KC_LOWER, KC_ENT,      KC_SPC,  KC_RAISE, KC_RCTRL, KC_RALT, KC_RGUI \
+// ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |      |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  | F10  | F11  |
@@ -85,10 +96,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------'           '------''---------------------------'
  */
 [_LOWER] = LAYOUT( \
-  KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_BSPC, \
-  KC_GRV, KC_1, KC_2, RALT(KC_8), RALT(KC_9), KC_GRV, KC_EXLM, KC_NUBS, LSFT(KC_NUBS), LSFT(KC_0), KC_F11, KC_F12, \
-  KC_TRNS, RALT(KC_MINS), KC_MINS, RALT(KC_7), RALT(KC_0), LSFT(KC_NUHS), LSFT(KC_MINS), LSFT(KC_8), LSFT(KC_9), KC_CIRC, RALT(KC_Q), KC_PIPE, \
-  KC_TRNS, KC_LBRC, KC_NUHS, RALT(KC_NUBS), KC_LCBR, KC_RCBR, KC_TRNS, KC_TRNS, KC_PERC, KC_AT, KC_SCLN, KC_COLN, KC_BSLS, KC_TRNS, \
+  KC_TRNS,   KC_F1,   KC_F2,  KC_F3,    KC_F4,  KC_F5,                  KC_F6,     KC_F7,     KC_F8,    KC_F9,       KC_F10,      KC_BSPC, \
+  KC_GRV,    KC_1,    KC_2,   RALT(KC_8),  RALT(KC_9), KC_GRV,          KC_EXLM,   KC_NUBS,   LSFT(KC_NUBS),     LSFT(KC_0),     KC_F11,    KC_F12, \
+  KC_TRNS,   RALT(KC_MINS), KC_MINS, RALT(KC_7), RALT(KC_0), LSFT(KC_NUHS), LSFT(KC_MINS), LSFT(KC_8), LSFT(KC_9), KC_CIRC, RALT(KC_Q), KC_PIPE, \
+  KC_TRNS,   KC_LBRC, KC_NUHS, RALT(KC_NUBS), KC_LCBR, KC_RCBR, KC_TRNS,    KC_TRNS,    KC_PERC,    KC_AT, KC_SCLN, KC_COLN, KC_BSLS, KC_TRNS, \
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(4), KC_TRNS, KC_TRNS, KC_TRNS \
 ),
 /* RAISE
@@ -127,7 +138,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------'           '------''---------------------------'
  */
   [_ADJUST] = LAYOUT( \
-  KC_TRNS, DF(5), DF(0), DF(1), CG_TOGG, RESET, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+  KC_TRNS, DF(2), DF(0), DF(1), CG_TOGG, RESET, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
   KC_NO, KC_F1, KC_F2, KC_F3, KC_F4, KC_NO, KC_NO, KC_F5, KC_F6, KC_F7, KC_F8, KC_NO, \
   KC_NO, KC_F9, KC_F10, KC_F11, KC_F12, KC_MPRV, KC_MNXT, KC_F13, KC_F14, KC_F15, KC_F16, KC_NO, \
   KC_TRNS, KC_F17, KC_F18, KC_F19, KC_F20, KC_MNXT, KC_TRNS, KC_TRNS, KC_MPRV, KC_F21, KC_F22, KC_F23, KC_F24, KC_TRNS, \
@@ -136,6 +147,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 #ifdef OLED_DRIVER_ENABLE
+
+void suspend_power_down_user(void) {
+    oled_off();
+}
 
 static void render_logo(void) {
     static const char PROGMEM qmk_logo[] = {
@@ -165,6 +180,9 @@ static void print_status_narrow(void) {
         case _COLEMAK:
             oled_write_ln_P(PSTR("Clmk"), false);
             break;
+        case _BONE:
+            oled_write_ln_P(PSTR("Bone"), false);
+            break;
         default:
             oled_write_P(PSTR("Undef"), false);
     }
@@ -173,6 +191,7 @@ static void print_status_narrow(void) {
     oled_write_ln_P(PSTR("LAYER"), false);
     switch (get_highest_layer(layer_state)) {
         case _COLEMAK:
+        case _BONE
         case _QWERTY:
             oled_write_P(PSTR("Base\n"), false);
             break;
